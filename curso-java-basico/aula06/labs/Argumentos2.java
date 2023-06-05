f6b49383d6eec5a3e63e0366d7efb220d1b33466
marciolod <marcio_lod@hotmail.com>
2023-06-04 23:45:22 -0300

    Loiane Groner Curso Java Básico.

---
+++ curso-java-basico/aula06/labs/Argumentos2.java
@@ -0,0 +1,10 @@
+class Argumentos2 {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		System.out.println("O que você está achando das aulas de java? " + args[0]);
+
+	}
+
+}
---
+++ curso-java-basico/aula07/Erro03.java
@@ -0,0 +1,11 @@
+class Erro03 {
+	
+	public static void main (String[] args) {
+
+		//System.out.println("Você dgitou: ");
+		System.out.println("Você dgitou: "+ 1/0);
+
+	}
+
+
+}
